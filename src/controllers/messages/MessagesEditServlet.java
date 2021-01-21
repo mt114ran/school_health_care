7fa5ddb20bdb1b775a7eaa39d63e0ea381f7a540
mt114ran <mt114r.an@gmail.com>
2021-01-21 14:17:10 +0900

    Add message edit

--- src/controllers/messages/MessagesEditServlet.java
+++ src/controllers/messages/MessagesEditServlet.java
@@ -1,7 +1,6 @@
 package controllers.messages;
 
 import java.io.IOException;
-import java.sql.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -14,19 +13,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Group;
 import models.Message;
+import models.User;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class ReportsNewServlet
+ * Servlet implementation class ReportsEditServlet
  */
-@WebServlet("/messages/new")
-public class MessagesNewServlet extends HttpServlet {
+@WebServlet("/messages/edit")
+public class MessagesEditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MessagesNewServlet() {
+    public MessagesEditServlet() {
         super();
     }
 
@@ -35,34 +35,32 @@ public class MessagesNewServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
-        request.setAttribute("_token", request.getSession().getId());
 
-        int page = 1;
-        try{
-            page = Integer.parseInt(request.getParameter("page"));
-        } catch(NumberFormatException e) { }
-
-        Message m = new Message();
-        m.setMessage_date(new Date(System.currentTimeMillis()));
-        request.setAttribute("message", m);
+        Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
 
         long groups_count = (long)em.createNamedQuery("getAllGroupsCount", Long.class)
-                .getSingleResult();
+                                        .getSingleResult();
 
         List<Group> groups = null;
 
         if(groups_count != 0){
             groups = em.createNamedQuery("getAllGroups",Group.class)
-            .setFirstResult(15 * (page - 1))
-            .setMaxResults(15)
-            .getResultList();
+                        .getResultList();
         }
 
         em.close();
 
-        request.getSession().setAttribute("groups_count", groups_count);
-        request.getSession().setAttribute("groups", groups);
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        User login_user = (User)request.getSession().getAttribute("login_user");
+        if(m != null && login_user.getId() == m.getUser().getId()) {
+            request.setAttribute("message", m);
+            request.getSession().setAttribute("groups", groups);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("message_id", m.getId());
+
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
         rd.forward(request, response);
     }
+
 }
